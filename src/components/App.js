75f7aad7c6b8de00d1985f9460686fea5e401c58
reaganongaya <reagan.ongaya@student.moringaschool.com>
2023-01-16 18:00:38 +0300

    Assignment completed

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,13 @@ import Home from "./Home";
 import About from "./About";
 
 // pass this data down as props to the child component(s) that need it!
+class App extends React.Component{
+  render(){
+    return <p> NavBar, {this.props.name}</p>;
+           
+  }
+}
+
 import user from "../data/user";
 console.log(user);
 
